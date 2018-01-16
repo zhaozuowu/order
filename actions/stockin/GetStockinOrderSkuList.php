a74fd072aa5f9d4a3ff90d7da6df493561e5578a
chenwende <chenwende@iwaimai.baidu.com>
2018-01-16 17:54:15 +0800

    reformat coding style and solve output type info and required time param validate

--- actions/stockin/GetStockinOrderSkuList.php
+++ actions/stockin/GetStockinOrderSkuList.php
@@ -49,18 +49,25 @@ class Action_GetStockinOrderSkuList extends Order_Base_Action
         $arrRetList = $arrRet['list'];
         foreach ($arrRetList as $arrListItem) {
             $arrRoundResult = [];
-            $arrRoundResult['sku_id'] = empty($arrListItem['sku_id']) ? '' : intval($arrListItem['sku_id']);
-            $arrRoundResult['upc_id'] = empty($arrListItem['upc_id']) ? '' : strval($arrListItem['upc_id']);
-            $arrRoundResult['sku_name'] = empty($arrListItem['sku_name']) ? '' : strval($arrListItem['sku_name']);
-            $arrRoundResult['upc_unit'] = empty($arrListItem['upc_unit']) ? '' : intval($arrListItem['upc_unit']);
+            $arrRoundResult['sku_id'] = empty($arrListItem['sku_id']) ? ''
+                : intval($arrListItem['sku_id']);
+            $arrRoundResult['upc_id'] = empty($arrListItem['upc_id']) ? ''
+                : strval($arrListItem['upc_id']);
+            $arrRoundResult['sku_name'] = empty($arrListItem['sku_name']) ? ''
+                : strval($arrListItem['sku_name']);
+            $arrRoundResult['upc_unit'] = empty($arrListItem['upc_unit']) ? ''
+                : intval($arrListItem['upc_unit']);
             $arrRoundResult['upc_unit_num'] = empty($arrListItem['upc_unit_num']) ? '' : intval($arrListItem['upc_unit_num']);
 //            $arrRoundResult['stockin_order_sku_total_price_tax'] = empty($arrListItem['stockin_order_sku_total_price_tax']) ? '' : intval($arrListItem['stockin_order_sku_total_price_tax']);
 //            $arrRoundResult['stockin_order_sku_total_price'] = empty($arrListItem['stockin_order_sku_total_price']) ? '' : intval($arrListItem['stockin_order_sku_total_price']);
 //            $arrRoundResult['sku_price'] = empty($arrListItem['sku_price']) ? '' : intval($arrListItem['sku_price']);
 //            $arrRoundResult['sku_price_tax'] = empty($arrListItem['sku_price_tax']) ? '' : intval($arrListItem['sku_price_tax']);
-            $arrRoundResult['reserve_order_sku_plan_amount'] = empty($arrListItem['reserve_order_sku_plan_amount']) ? '' : intval($arrListItem['reserve_order_sku_plan_amount']);
-            $arrRoundResult['stockin_order_sku_real_amount'] = empty($arrListItem['stockin_order_sku_real_amount']) ? '' : intval($arrListItem['stockin_order_sku_real_amount']);
-            $arrRoundResult['stockin_order_sku_extra_info'] = empty($arrListItem['stockin_order_sku_extra_info']) ? '' : strval($arrListItem['stockin_order_sku_extra_info']);
+            $arrRoundResult['reserve_order_sku_plan_amount'] = empty($arrListItem['reserve_order_sku_plan_amount']) ? ''
+                : intval($arrListItem['reserve_order_sku_plan_amount']);
+            $arrRoundResult['stockin_order_sku_real_amount'] = empty($arrListItem['stockin_order_sku_real_amount']) ? ''
+                : intval($arrListItem['stockin_order_sku_real_amount']);
+            $arrRoundResult['stockin_order_sku_extra_info'] = empty($arrListItem['stockin_order_sku_extra_info']) ? ''
+                : strval($arrListItem['stockin_order_sku_extra_info']);
 
             $arrFormatResult['list'][] = $arrRoundResult;
         }
