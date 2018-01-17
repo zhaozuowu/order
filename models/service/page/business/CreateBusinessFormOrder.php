9c14a9edfe0fdca57a3e61dc1ab8ae57e2e96622
jinyu02 <jinyu02@iwaimai.baidu.com>
2018-01-17 17:57:11 +0800

    stockout
    Change-Id: I292d1957ed46274f5ca1fccc4809526ca48b0f31

--- models/service/page/business/CreateBusinessFormOrder.php
+++ models/service/page/business/CreateBusinessFormOrder.php
@@ -43,8 +43,6 @@ class Service_Page_Business_CreateBusinessFormOrder {
                                                 $arrInput['business_form_order_type']);
         $arrInput = $this->objDsStockoutFormOrder->assembleStockoutOrder($arrInput);
         $arrInput = $this->objDsBusinessFormOrder->createBusinessFormOrder($arrInput);
-        $this->objDsStockoutFormOrder->createStockoutOrder($arrInput);
-        return true;
         //异步创建出库单
         $ret = Order_Wmq_Commit::sendWmqCmd(Order_Define_Cmd::CMD_CREATE_STOCKOUT_ORDER, $arrInput,
                                             strval($arrInput['stockout_order_id']));
