834be88fbd64cb91b4fd30fa375dcfa57de991f2
lvbochao <lvbochao@iwaimai.baidu.com>
2018-01-24 16:43:53 +0800

    fix null

--- models/orm/StockinOrderSku.php
+++ models/orm/StockinOrderSku.php
@@ -72,6 +72,8 @@ class Model_Orm_StockinOrderSku extends Order_Base_Orm
                 'sku_price' => intval($arrRow['sku_price']),
                 'sku_price_tax' => intval($arrRow['sku_price_tax']),
                 'sku_tax_rate' => intval($arrRow['sku_tax_rate'] ?? 0),
+                'sku_effect_type' => intval($arrRow['sku_effect_type'] ?? 0),
+                'sku_effect_day' => intval($arrRow['sku_effect_day'] ?? 0),
                 'stockin_order_sku_total_price' => intval($arrRow['stockin_order_sku_total_price']),
                 'stockin_order_sku_total_price_tax' => intval($arrRow['stockin_order_sku_total_price_tax']),
                 'reserve_order_sku_plan_amount' => intval($arrRow['reserve_order_sku_plan_amount']),
