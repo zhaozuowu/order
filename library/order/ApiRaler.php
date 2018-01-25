5bafdde38030f603975ecbb537653882d693f69c
lvbochao <lvbochao@iwaimai.baidu.com>
2018-01-25 14:32:04 +0800

    add nscminbouddirect

--- library/order/ApiRaler.php
+++ library/order/ApiRaler.php
@@ -86,4 +86,24 @@ class Order_ApiRaler extends Nscm_lib_ApiRaler
         return $data;
     }
 
+    /**
+     * @param $data
+     * @return mixed
+     * @throws Order_BusinessError
+     */
+    protected function nscminbouddirect($data)
+    {
+        if (empty($data) || ($data['error_no'] != 0)) {
+            Bd_Log::warning(sprintf(
+                '[%s] request service exception req_info[%s] error[%s] msg[%s]',
+                __METHOD__,
+                json_encode($this->arrCurrentReq),
+                $data['error_no'],
+                $data['error_msg']
+            ));
+            Order_BusinessError::throwException(Order_Error_Code::RAL_ERROR);
+        }
+        return $data;
+    }
+
 }
