9e4c86fdf2776b67a2515aabdb71abf6c67894c2
jinyu02 <jinyu02@iwaimai.baidu.com>
2018-03-05 14:28:03 +0800

    stockout
    Change-Id: I27ed441369bf6c53fe118cddd3c4b54546e9e294

--- models/service/data/StockoutOrder.php
+++ models/service/data/StockoutOrder.php
@@ -592,6 +592,7 @@ class Service_Data_StockoutOrder
             $userName = !empty($userName) ? $userName:Order_Define_Const::DEFAULT_SYSTEM_OPERATION_NAME ;
             $this->addLog($userId, $userName, '完成拣货:'.$strStockoutOrderId.",拣货数量:".$stockoutOrderPickupAmount, $operationType, $strStockoutOrderId);
             $this->notifyTmsFnishPick($strStockoutOrderId,$pickupSkus);
+
         });
         Dao_Ral_Statistics::syncStatistics(Order_Statistics_Type::TABLE_STOCKOUT_ORDER,
             Order_Statistics_Type::ACTION_UPDATE,
