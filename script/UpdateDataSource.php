a351c87dae2297ed47a27acd5f0094eb37f67926
lvbochao <bochao.lv@ele.me>
2018-03-20 12:44:16 +0800

    add update data source

---
+++ script/UpdateDataSource.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @name UpdateDataSource
+ * @desc update data source to business
+ * @author lvbochao@iwaimai.baidu.com
+ */
+Bd_Init::init();
+    $objUpdateDataSource = new UpdateDataSource();
+    $objUpdateDataSource->run();
+    Bd_Log::trace('complete');
+try {
+
+} catch (Exception $e) {
+    Bd_Log::warning(sprintf('exec %s error. code[%d], msg[%s]',
+        __FILE__, $e->getCode(), $e->getMessage()));
+    exit(-1);
+}
+
+
+
+class UpdateDataSource
+{
+    const LIMIT = 20;
+    /**
+     * run
+     */
+    public function run()
+    {
+        // start time
+        $intStartTime = time();
+        $arrCondition = [
+            'data_source' => 0,
+            'create_time' => ['<', $intStartTime]
+        ];
+        $arrOrderBy = [
+            'id' => 'asc',
+        ];
+        $intOffset = 0;
+        $arrNewColumn = [
+            'data_source' => Order_Define_StockoutOrder::STOCKOUT_DATA_SOURCE_SYSTEM_ORDER,
+        ];
+        $arrAllIds = Model_Orm_StockoutOrder::findColumn('id', $arrCondition);
+        Bd_Log::trace(sprintf('all ids total[%d]', $intOffset, self::LIMIT, count($arrAllIds)));
+        do {
+            $arrIds = array_slice($arrAllIds, $intOffset, self::LIMIT);
+            if (empty($arrIds)) {
+                break;
+            }
+            $arrUpdateCondition = [
+                'id' => ['in', $arrIds],
+            ];
+            Model_Orm_StockoutOrder::updateAll($arrNewColumn, $arrUpdateCondition);
+            Bd_Log::trace(sprintf('batch update finish. id:[%s]', implode(', ', $arrIds)));
+            $intOffset += self::LIMIT;
+            sleep(1);
+        } while (!empty($arrIds));
+    }
+}
