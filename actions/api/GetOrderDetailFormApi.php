0b7155f55256441560f8cf7990479577efe11b71
lihui04_iwaimai.baidu.com_waimai <lihui04@iwaimai.baidu.com>
2018-03-27 16:23:42 +0800

    需改报表ui展示和导出两个接口质量状态字段

--- actions/api/GetOrderDetailFormApi.php
+++ actions/api/GetOrderDetailFormApi.php
@@ -90,6 +90,8 @@ class Action_GetOrderDetailFormApi extends Order_Base_ApiAction
             $arrFormatDetail['adjust_amount']    = empty($detail['adjust_amount']) ? '' : strval($detail['adjust_amount']);
             $arrFormatDetail['unit_price']    = empty($detail['unit_price']) ? '' : Nscm_Service_Price::convertDefaultToYuan($detail['unit_price']);
             $arrFormatDetail['unit_price_tax']    = empty($detail['unit_price_tax']) ? '' : Nscm_Service_Price::convertDefaultToYuan($detail['unit_price_tax']);
+            $arrFormatDetail['is_defective']    = $detail['is_defective'];
+            $arrFormatDetail['is_defective_text']    = $detail['is_defective'] == Order_Define_Const::HIGH_QUALITY ? Order_Define_Const::QUALITY_TEXT[Order_Define_Const::HIGH_QUALITY] : Order_Define_Const::QUALITY_TEXT[Order_Define_Const::INFERIOR_QUALITY];
 
             if(empty($detail['adjust_amount']) || empty($detail['unit_price']) || empty($detail['unit_price_tax'])) {
                 $arrFormatResult['total_unit_price']    = '';
