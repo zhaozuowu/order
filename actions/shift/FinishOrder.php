9ac4fde16e050d06e07c66483f832b6f49a0a5b0
songwenkai_iwm <songwenkai@iwaimai.baidu.com>
2018-05-21 17:15:28 +0800

    songwenkai
    Change-Id: I610d46c5f9d26a06a77349386262bef04ce13a9c

---
+++ actions/shift/FinishOrder.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @name Action_Createincreaseorder
+ * @desc 创建库存调整单-调增
+ * @author sunzhixin@iwaimai.baidu.com
+ */
+
+class Action_CreateOrder extends Order_Base_Action
+{
+    /**
+     * input params
+     * @var array
+     */
+    protected $arrInputParams = [
+        'warehouse_id'      => 'int|required',
+        'source_location'   => 'str|required',
+        'target_location'   => 'str|required',
+        'detail'            => [
+            'validate'      => 'json|required|decode',
+            'type'          => 'array',
+            'params'        => [
+                'sku_id'                => 'int|required',
+                'sku_name'              => 'str|required',
+                'upc_id'                => 'str|required|min[1]|len[64]',
+                'upc_unit'              => 'int|required',
+                'upc_unit_num'          => 'int|required',
+                'production_time'       => 'int|required',
+                'expiration_time'       => 'int|required',
+                'shift_amount'          => 'int|required',
+                'is_defective'          => 'int|required',
+            ],
+        ],
+    ];
+
+    /**
+     * method
+     * @var int
+     */
+    protected $intMethod = Order_Define_Const::METHOD_POST;
+
+    /**
+     * page service
+     * @var Service_Page_Adjust_CreateOrder
+     */
+    protected $objPage;
+
+    /**
+     * init object
+     */
+    public function myConstruct()
+    {
+        $this->objPage = new Service_Page_Shift_CreateOrder();
+    }
+
+    /**
+     * format result
+     * @param array $data
+     * @return array
+     */
+    public function format($data)
+    {
+        return $data;
+        $arrFormatResult                          = [];
+        $arrFormatResult['shift_order_id'] = empty($data['shift_order_id']) ? '' : Nscm_Define_OrderPrefix::SHO . intval($data['shift_order_id']);
+
+        return $arrFormatResult;
+    }
+}
---
+++ models/service/page/shift/FinishOrder.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @name Service_Page_Adjust_CreateOrder
+ * @desc 新建调整单
+ * @author sunzhixin@iwaimai.baidu.com
+ */
+
+class Service_Page_Shift_CreateOrder
+{
+    /**
+     * @var Service_Data_StockAdjustOrder
+     */
+    protected $objStockAdjustOrder;
+
+    /**
+     * @var Service_Data_StockAdjustOrderDetail
+     */
+    protected $objStockAdjustOrderDetail;
+
+    /**
+     * init
+     */
+    public function __construct()
+    {
+        $this->objShiftOrder = new Service_Data_ShiftOrder();
+    }
+
+    /**
+     * execute
+     * @param  array $arrInput 参数
+     * @return array
+     */
+    public function execute($arrInput)
+    {
+        $arrInput['warehouse_id'] = '1';
+        $arrInput['source_location'] = '1';
+        $arrInput['target_location'] = '1';
+        $arrInput['detail'] = array();
+        $arrInput['detail'][]=array(
+            'sku_id'=>1,
+            'sku_name'=>'2',
+            'upc_id'=>1,
+            'upc_unit'=>1,
+            'upc_unit_num'=>1,
+            'production_time'=>1,
+            'expiration_time'=>1,
+            'shift_amount'=>1,
+            'is_defective'=>1,
+        );
+        // 生成一个移位单号
+        $arrInput['shift_order_id'] = Order_Util_Util::generateShiftOrderId();
+        Bd_Log::trace('generate shift order id: ' . $arrInput['shift_order_id']);
+
+        // 创建移位单
+        $arrOutput = $this->objShiftOrder->createShiftOrder($arrInput);
+        return $arrOutput;
+    }
+}
