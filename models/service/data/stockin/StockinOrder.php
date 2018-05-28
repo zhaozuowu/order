42608fe20a5fa4fb715b641aa6fb2c91fd50f9c8
songhang <hang.song02@ele.me>
2018-05-28 21:07:44 +0800

    创建上架单

--- models/service/data/stockin/StockinOrder.php
+++ models/service/data/stockin/StockinOrder.php
@@ -1327,7 +1327,7 @@ class Service_Data_Stockin_StockinOrder
             if (!empty($arrStockInOrderInfo['shipment_order_id'])) {
                 $this->sendConfirmStockinOrderInfoToOms($intStockInOrderId, $arrStockInOrderInfo['shipment_order_id'], $arrStockInOrderInfo['stockin_order_source'], $arrSkuInfoList);
             }
-            $arrInput['stockin_order_ids'] = $strStockInOrderId;
+            $arrInput['stockin_order_ids'] = $intStockInOrderId;
             $ret = Order_Wmq_Commit::sendWmqCmd(Order_Define_Cmd::CMD_PLACE_ORDER_CREATE, $arrInput);
             if (false == $ret) {
                 Bd_Log::warning("send wmq failed arrInput[%s] cmd[%s]",
