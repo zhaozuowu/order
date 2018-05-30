29fad2c9b956111a6cc2330588177993d9f6f3ba
wende.chen <wende.chen@ele.me>
2018-05-30 20:29:15 +0800

    remove unused controller item

--- controllers/Rf.php
+++ controllers/Rf.php
@@ -15,7 +15,6 @@ class Controller_Rf extends Ap_Controller_Abstract {
         'saveorderoperaterecordstockin' => 'actions/main/SaveOrderOperateRecordStockin.php', #rf保存销退入库单操作纪录#
         'saveorderoperaterecordreserve' => 'actions/main/SaveOrderOperateRecordReserve.php', #rf保存预约单操作纪录#
         'getstockinstockoutorderlist' => 'actions/stockin/GetStockinStockoutOrderList.php', #rf查询销退入库单列表#
-//        'getstockinorderlist' => 'actions/stockin/GetStockinOrderList.php', #rf查询入库单列表#
         'getstockinorderdetail' => 'actions/stockin/GetStockinOrderDetail.php', #rf获取入库单详情#
         'getstockinorderskulist' => 'actions/stockin/GetStockinOrderSkuList.php', #rf获取入库单商品列表#
         'getstockinskubyordersku' => 'actions/stockin/GetStockinSkuByOrderSku.php', #rf根据入库单号和扫码查询商品信息#,
