fccd9e63689b2ae1382f1b2bd8aba1a6cf3b22c9
lvbochao <bochao.lv@ele.me>
2018-05-31 18:10:01 +0800

    add text

---
+++ library/order/define/Text.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @name Text.php
+ * @desc
+ * @author: bochao.lv@ele.me
+ * @createtime: 2018/5/31 17:15
+ */
+
+class Order_Define_Text
+{
+    /**
+     * start operate stockin order
+     */
+    const START_OPERATE_STOCKIN_ORDER = '开始操作此单据';
+
+    const SUBMIT_STOCKIN_RESERVE_ORDER = '提交了预约入库单';
+
+    const SUBMIT_STOCKIN_STOCKOUT_ORDER = '提交了销退入库单';
+}
