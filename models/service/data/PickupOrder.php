983944f2163d6b246415741269a3315a9cb46721
songhang <hang.song02@ele.me>
2018-06-01 00:18:30 +0800

    拣货完成，存储信息

--- models/service/data/PickupOrder.php
+++ models/service/data/PickupOrder.php
@@ -654,7 +654,7 @@ class Service_Data_PickupOrder
         foreach ($arrPickupSkus as $arrSkuInfo) {
             $arrUpdateFields[] = [
                 'pickup_amount' => $arrSkuInfo['pickup_amount'],
-                'pickup_extra_info' => json_encode($arrSkuInfo['pickup_extra_info']),
+                'pickup_extra_info' => json_encode($arrSkuInfo['pick_infos']),
             ];
             $arrUpdateCondition[] = [
                 'pickup_order_id' => $intPickupOrderId,
